a9bdf37224b87731c7e6e442bd5f4e42a38d22ee
Dipon De <126169712+Dipon-De@users.noreply.github.com>
2026-09-13 20:00:00 +0530

    Revert "Install and configure Vercel Web Analytics"

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import {
   Navigate
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import { Analytics } from "@vercel/analytics/react";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -44,7 +43,6 @@ function App() {
         </Routes>
         <Footer />
       </div>
-      <Analytics />
     </Router>
   );
 }
